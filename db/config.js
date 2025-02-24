348ea11c40d87c43bc0cd85525eb8b491ee7993c
Francisco Sabaté <81888471+dariopirozzo@users.noreply.github.com>
2025-02-24 19:58:35 -0300

    Update config.js

--- db/config.js
+++ db/config.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const dbConnection =async ()=>{
     try {
-     await   mongoose.connect(process.env.DB_CNN);
+     await   mongoose.connect(process.env.DB_CNN,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, // Espera 5 segundos antes de fallar
+      });
      console.log('db online')
     } catch (error) {
         console.log(error)
